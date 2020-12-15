1093ad54226e4b4afc98b95ad6f1f097bef7fcce
nunorpj <nunorpj@gmail.com>
2020-12-15 23:19:46 +0000

    Sub app Execution Context
    when running a micro FE isolated we may want to immediately render our app into a element but in the container app we dont that guarantee that the element exists so we dont want to run our app immediately.

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,36 @@
 import faker from "faker";
 
-let products = "";
+const mount = (el) => {
+  let products = "";
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+//  Context #1
+//  We are running this file in development isolation
+//  We are using our local index.html file
+//  Which DEFINITELY has an element with an id "dev-products"
+//  We want to immediately render our app into that element
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-only-products");
+
+  // Assuming our container app doesn't have this element
+  // with the id "#dev-only-products" (this is a BIG assumption)
+  if (el) {
+    // we are probably running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+//  Context #2
+//  We are running this file in development or production
+//  through the CONTAINER app
+//  NO GUARANTEE that an element with an id "dev-products" exists
+//  WE DO NOT WANT try immediately render the app
+
+export { mount };
