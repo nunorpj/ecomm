9ec036b5b6461d2e1168545444bf3ba84131d207
nunorpj <nunorpj@gmail.com>
2020-12-15 00:40:27 +0000

    enabling module federation

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,10 @@
-import faker from "faker";
+import faker from 'faker';
 
-let products = "";
+let products = '';
 
 for (let i = 0; i < 5; i++) {
   const name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 }
 
-document.querySelector("#dev-products").innerHTML = products;
+document.querySelector('#dev-products').innerHTML = products;
